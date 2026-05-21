d75bdddd77bf9c31ff3bd2dacaa45d270d4fd1f3
Jhon Bueno <jhonbuenoa@gmail.com>
2026-05-21 03:08:26 -0500

    feat(utilidades): agregar setupPasswordToggle para alternar visibilidad de contrasenas

--- src/utils/ui.utils.js
+++ src/utils/ui.utils.js
@@ -7,3 +7,41 @@ export const hideContainer = (container) => {
 export const showContainer = (container) => {
     container.classList.remove('hidden');
 }
+
+
+/**
+ * Alterna la visibilidad de un input password.
+ * 
+ * @param {string} buttonId - id HTML del boton inicializador
+ * @param {string} inputId  - id HTML del input
+ */
+export const setupPasswordToggle = (buttonId, inputId) => {
+
+    const button = document.querySelector(`#${buttonId}`);
+
+    const input = document.querySelector(`#${inputId}`);
+
+    const eyeShow = button.querySelector(".eye-show");
+
+    const eyeHide = button.querySelector(".eye-hide");
+
+    button.addEventListener("click", () => {
+
+        const isPassword =
+            input.type === "password";
+
+        input.type = isPassword
+            ? "text"
+            : "password";
+
+        eyeShow.style.display = isPassword
+            ? "none"
+            : "block";
+
+        eyeHide.style.display = isPassword
+            ? "block"
+            : "none";
+
+    });
+
+};
