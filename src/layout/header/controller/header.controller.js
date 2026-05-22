6e1229ef83bc8c25ec907c148779567429aed654
Jhon Bueno <jhonbuenoa@gmail.com>
2026-05-21 22:38:06 -0500

    feat(layout): navegacion dinamica filtrada por permisos de usuario

--- src/layout/header/controller/header.controller.js
+++ src/layout/header/controller/header.controller.js
@@ -1,18 +1,34 @@
-import { clearTokens, navigateTo } from '../../../utils/index.js';
+import { clearTokens, navigateTo, showConfirm, showConfirmThenAlert, showToast } from '../../../utils/index.js';
+import { headerLayout } from '../view/header.view.js';
+import { generateNavItems } from './navItems.controller.js';
+
 
 export const headerInit = () => {
 
     // ========================================================
-    //                       EVENTOS
+    //                 SELECTORES DEL DOM
     // ========================================================
+    const btnLogout = document.querySelector('#btn-logout');
 
-    // Cerrar sesión
-    document.querySelector('#btn-logout')
-        ?.addEventListener('click', () => {
-            clearTokens();
-            localStorage.removeItem('user');
-            navigateTo('#/login');
-        });
-
-
+    // ========================================================
+    //                  EVENTO => CERRAR SESIÓN
+    // ========================================================
+    btnLogout.addEventListener('click', async () => {
+        const confirmed = await showConfirmThenAlert(
+            "¿Cerrar sesión?",
+            "Se cerrará tu sesión actual.",
+            "¡Hasta pronto!",
+            "Sesión cerrada exitosamente.",
+            "Confirmar",
+            "Cancelar",
+            () => {
+                clearTokens();
+                localStorage.removeItem('user');
+                navigateTo('#/login');
+            }
+        );
+    });
 }
+
+
+
--- src/layout/index.js
+++ src/layout/index.js
@@ -2,6 +2,10 @@
 //                      ARCHIVO BARRIL
 // ========================================================
 
+// VIEWS
 export * from "./header/view/header.view";
+export * from './header/view/navItems.view'
 
+// CONTROLLERS
 export * from "./header/controller/header.controller";
+export * from './header/controller/navItems.controller'
